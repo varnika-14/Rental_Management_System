0b52c79187e75042bce4733d74a814176ae69594
varnika-14 <varnikapathuri1410@gmail.com>
2026-04-24 00:18:14 +0530

    updated api url in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : "http://localhost:5000/api";
+
 const API = axios.create({
-  baseURL: "https://rental-backend-sh0x.onrender.com",
+  baseURL: baseURL,
 });
 
 API.interceptors.request.use((req) => {
